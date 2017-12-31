f04fcd9bfd97fedce1971a175c4e5f7a0979ce5a
StarixWoS <StarixWoS@gmail.com>
2017-12-30 18:17:04 -0600

    no message

---
+++ src/widgets/commentviewwidget.hpp
@@ -0,0 +1,26 @@
+#ifndef COMMENTVIEWWIDGET_HPP
+#define COMMENTVIEWWIDGET_HPP
+
+#include <QWidget>
+
+namespace Ui {
+    class CommentViewWidget;
+}
+
+class CommentViewWidget : public QWidget
+{
+    Q_OBJECT
+
+    public:
+        explicit CommentViewWidget(QWidget *parent = 0);
+        ~CommentViewWidget();
+
+        void setTitle(const QString& name);
+        void insertComment(const QString& sernum, const QString& alias,
+                           const QString& message);
+
+    private:
+        Ui::CommentViewWidget *ui;
+};
+
+#endif // COMMENTVIEWWIDGET_HPP
